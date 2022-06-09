462a87b5291d1d6c8fdb92b5d49b35ed6ac9416c
kirill <kir.yakimov@mail.ru>
2022-06-09 18:43:07 +0300

    18:42 09.06.2022

---
+++ src/main/java/Student.java
@@ -0,0 +1,45 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+    @Column(name = "registration_date")
+    private String registrationDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
